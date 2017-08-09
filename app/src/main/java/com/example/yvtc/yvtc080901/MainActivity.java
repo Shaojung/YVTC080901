09e55ee28f7be1dda4a00e216de7bb1385e1f7f5
Shaojung <shaojung@gmail.com>
2017-08-09 10:45:20 +0800

    2 data

--- app/src/main/java/com/example/yvtc/yvtc080901/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc080901/MainActivity.java
@@ -14,17 +14,20 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    EditText ed1;
+    EditText ed1, ed2;
     FirebaseDatabase database;
     DatabaseReference myRef;
+    DatabaseReference myRef2;
     final String TAG = "T0809";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ed1 = (EditText) findViewById(R.id.editText);
+        ed2 = (EditText) findViewById(R.id.editText2);
         database = FirebaseDatabase.getInstance();
         myRef = database.getReference("message");
+        myRef2 = database.getReference("message2");
 
         // Read from the database
         myRef.addValueEventListener(new ValueEventListener() {
@@ -44,11 +47,33 @@ public class MainActivity extends AppCompatActivity {
                 Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
+        myRef2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                TextView tv2 = (TextView) findViewById(R.id.textView2);
+                tv2.setText(value);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
     public void clickUpdate(View v)
     {
 
 
         myRef.setValue(ed1.getText().toString());
+
+    }
+    public void click2(View v)
+    {
+        myRef2.setValue(ed2.getText().toString());
     }
 }
